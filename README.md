e37a1e52012e8e33c7af09faf972ee578ce36768
aclec <anicetprodhomme@gmail.com>
2021-04-13 15:30:09 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,105 @@
+# parse-mail-adapter-ejs
+Parse Server Mail Adapter with EJS templates
+
+### Installation
+
+Install npm module in your parse server project
+
+```sh
+$ npm install --save parse-mail-adapter-smtp-ejs
+```
+
+### Use
+
+
+```js
+"use strict";
+
+const Express = require('express');
+const ParseServer = require('parse-server').ParseServer;
+
+const app = Express();
+const APP_PORT = 8080;
+
+
+let api = new ParseServer({
+	appName: "Parse Test",
+	appId: "appid",
+	masterKey: "secret",
+	serverURL: "http://localhost:3000/parse",
+	publicServerURL: "http://localhost:3000/parse",
+	databaseURI: "mongodb://user:pass@host:27017/parse",
+	port: APP_PORT,
+	//This is the config for email adapter
+	emailAdapter: {
+		module: "parse-mail-adapter-smtp-ejs",
+		options: {
+			from: 'your@sender.address',
+			user: 'email@email.com',
+			password: 'AwesomePassword',
+			host: 'your.smtp.host',
+			secure: true, //True or false if you are using ssl 
+			port: 465,
+    		locales: ["en", "fr"],
+    		defaultLocale: "en",
+    		templatesDir: __dirname + '/templates/',
+			templates: {
+			    //This template is used only for reset password email
+				resetPassword: {
+				    //Path to your template
+					template:'reset-password.html',
+					//Subject for this email
+					subject: 'Reset your password'
+				},
+				verifyEmail: {
+				    template: 'verify-email.html',
+				    subject: 'Verify Email'
+				}
+			}
+		}
+	}
+});
+
+
+app.use('/parse', api);
+
+app.listen(APP_PORT, function () {
+	console.log(`Parse Server Ready and listening on port ${APP_PORT}`);
+});
+```
+
+### Template
+
+#### Architecture
+
+```js
+templates/
+└── en/                 // en locale folder
+│   └── example.html    // en localized file
+└── fr/                 // fr locale folder
+│   └── example.html    // fr localized file
+```
+
+#### Variables (resetPassword & verifyMail)
+- appName //This is the name of your parse app
+- link //This is the link for reset the password
+- user //This is a Parse object of the current user
+
+
+### Send Mail From Cloud Code
+
+=> Parse Server > 4.5.0
+```js
+Parse.Cloud.sendEmail({
+    template: "myTemplate.html", // Email Html
+    locale: "en",
+    from: "your@sender.address",
+    to: "user@email.address",
+    subject: "my Subejct",
+    text: "", // Email Text
+    data: {} // data gives to ejs
+});
+```
+
+
+### License MIT
---
+++ index.js
@@ -0,0 +1,169 @@
+"use strict";
+const path = require('path');
+const nodemailer = require("nodemailer");
+const ejs = require('ejs');
+
+let MailAdapter = (adapterOptions) => {
+
+    // Create transporter for send emails with SMTP
+    let transporter = nodemailer.createTransport({
+        host: adapterOptions.host,
+        port: adapterOptions.port,
+        secure: adapterOptions.secure,
+        auth: {
+            user: adapterOptions.user,
+            pass: adapterOptions.password
+        }
+    });
+
+
+    // Core Functions
+
+    let getUserEmail = (user) => {
+        return user.get('email') || user.get('username');
+    };
+
+    // Render templates with ejs
+    let renderTemplate = (template, data, locale) => {
+        let templatePath = adapterOptions.templatesDir;
+        if( adapterOptions.locales.indexOf(locale) > -1 ){
+            templatePath += `${locale}/${template}`
+        }else {
+            templatePath += `${adapterOptions.defaultLocale}/${template}`
+        }
+
+        return new Promise((resolve, reject) => {
+            ejs.renderFile(templatePath, data, function(err, str){
+                if (err) {
+                    console.log(err)
+                    reject(err);
+                } else {
+                    resolve(str);
+                }
+            });
+        });
+    };
+
+    // Default function to Send mails
+    let _sendMail = (mail) => {
+        let mailOptions = {
+            from: mail.from,
+            to: mail.to,
+            subject: mail.subject,
+            html: mail.html || "",
+            text: mail.text || ""
+        };
+
+        return new Promise((resolve, reject) => {
+            transporter.sendMail(mailOptions, (err, info) => {
+                if(err) {
+                    console.log(err)
+                    reject(err);
+                } else {
+                    resolve(info);
+                }
+            });
+        });
+    };
+
+
+    // Exported module functions
+
+
+    // Function fo send custom mails
+    let sendEmail = (options) => {
+        let mail = {
+            from: options.from,
+            to: options.to,
+            subject: options.subject,
+            text: options.text || ""
+        };
+
+        if(options.template){
+            return renderTemplate(options.template, options.data, options.locale).then((result) => {
+                mail.html = result;
+
+                return _sendMail(mail);
+            }).catch((e) => {
+
+                return new Promise((resolve, reject) => {
+                    console.log(e)
+                    reject(e);
+                });
+            });
+
+        }else {
+            throw "Error"
+        }
+    };
+
+
+    // Specific function for reset password.  data => {appName, link, user} user is Parse Object
+    let sendPasswordResetEmail = (data) => {
+        let mail = {
+            from: adapterOptions.from,
+            subject: 'Reset Password',
+            to: getUserEmail(data.user)
+        };
+
+        if (adapterOptions.templates && adapterOptions.templates.resetPassword) {
+
+            return renderTemplate(adapterOptions.templates.resetPassword.template, data, data.user.get("locale")).then((result) => {
+                mail.html = result;
+                mail.subject = adapterOptions.templates.resetPassword.subject;
+
+                return _sendMail(mail);
+            }).catch((e) => {
+
+                return new Promise((resolve, reject) => {
+                    console.log(e)
+                    reject(e);
+                });
+            });
+
+        } else {
+            mail.text = data.link;
+
+            return _sendMail(mail);
+        }
+    };
+
+
+    // Specific function for verify email.  data => {appName, link, user}  user is Parse Object
+    let sendVerificationEmail = (data) => {
+        let mail = {
+            from: adapterOptions.from,
+            subject: 'Verify Email',
+            to: getUserEmail(data.user)
+        };
+
+        if (adapterOptions.templates && adapterOptions.templates.verifyEmail) {
+
+            return renderTemplate(adapterOptions.templates.verifyEmail.template, data, data.user.get("locale")).then((result) => {
+                mail.html = result;
+                mail.subject = adapterOptions.templates.verifyEmail.subject;
+
+                return _sendMail(mail);
+            }).catch((e) => {
+
+                return new Promise((resolve, reject) => {
+                    console.log(e);
+                    reject(e);
+                });
+            });
+
+        } else {
+            mail.text = data.link;
+
+            return _sendMail(mail);
+        }
+    };
+
+    return Object.freeze({
+        sendMail: sendEmail,
+        sendPasswordResetEmail: sendPasswordResetEmail,
+        sendVerificationEmail: sendVerificationEmail
+    });
+};
+
+module.exports = MailAdapter;
