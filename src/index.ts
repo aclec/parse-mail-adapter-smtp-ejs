6575c11944a1ec5f993768a72f3cf0603c4c16f7
aclec <anicetprodhomme@gmail.com>
2022-01-12 15:10:15 +0100

    put Email function in index

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,124 @@
-import {AdapterOptionsInterface} from "./types";
+import {AdapterOptionsInterface, MailOptionsInterface, ParseDataEmailInterface} from "./types";
 import {createTransporter} from "./functions/createTransporter";
-import {sendEmail} from "./functions/email/sendEmail";
-import {sendPasswordResetEmail} from "./functions/email/sendPasswordResetEmail";
-import {sendVerificationEmail} from "./functions/email/sendVerificationEmail";
+import {renderTemplate} from "./functions/renderTemplate";
+import {_sendMail} from "./functions/_sendMail";
 
 let MailAdapter = (adapterOptions: AdapterOptionsInterface) => {
 
+    // --- Init Transporter ---
     const transporter = createTransporter(adapterOptions.nodeMailerOptions);
 
+    // --- Email Functions ---
+
+    // Email
+    const sendEmail = ( options: MailOptionsInterface ) => {
+
+        if(options.template){
+            return renderTemplate(options.template, options.data, options.locale, adapterOptions).then((result) => {
+
+                let mail = {
+                    from: options.from,
+                    to: options.to,
+                    subject: options.subject,
+                    text: options.text || "",
+                    html: result
+                };
+
+                return _sendMail(mail, transporter);
+            }).catch((e) => {
+
+                return new Promise((resolve, reject) => {
+                    console.log(e)
+                    reject(e);
+                });
+            });
+
+        }else {
+            throw "Error"
+        }
+    };
+
+    // Password
+    const sendPasswordResetEmail = ( data: ParseDataEmailInterface ) => {
+
+        if (adapterOptions?.templatesOptions?.templates && adapterOptions?.templatesOptions?.templates?.resetPassword) {
+
+            return renderTemplate(
+                adapterOptions.templatesOptions.templates.resetPassword.template,
+                {...data, user: data.user.get('email') || data.user.get('username')},
+                data.user.get("locale"),
+                adapterOptions
+
+            ).then((result) => {
+
+                return _sendMail({
+                    from: adapterOptions.defaultFrom,
+                    subject: adapterOptions.templatesOptions.templates.resetPassword?.subject || 'Reset Password',
+                    to: data.user.get('email') || data.user.get('username'),
+                    html: result,
+                }, transporter);
+
+            }).catch((e) => {
+
+                return new Promise((resolve, reject) => {
+                    console.log(e)
+                    reject(e);
+                });
+            });
+
+        } else {
+
+            return _sendMail({
+                from: adapterOptions.defaultFrom,
+                subject: adapterOptions.templatesOptions.templates.resetPassword?.subject || 'Reset Password',
+                to: data.user.get('email') || data.user.get('username'),
+                text: data.link,
+            }, transporter);
+        }
+    }
+
+    // Email Verification
+    const sendVerificationEmail = ( data: ParseDataEmailInterface ) => {
+
+        if (adapterOptions?.templatesOptions?.templates && adapterOptions?.templatesOptions?.templates?.resetPassword) {
+
+            return renderTemplate(
+                adapterOptions.templatesOptions.templates.verifyEmail.template,
+                {...data, user: data.user.get('email') || data.user.get('username')},
+                data.user.get("locale"),
+                adapterOptions
+
+            ).then((result) => {
+
+                return _sendMail({
+                    from: adapterOptions.defaultFrom,
+                    subject: adapterOptions.templatesOptions.templates.verifyEmail?.subject || 'Verify Email',
+                    to: data.user.get('email') || data.user.get('username'),
+                    html: result,
+                }, transporter);
+
+            }).catch((e) => {
+
+                return new Promise((resolve, reject) => {
+                    console.log(e)
+                    reject(e);
+                });
+            });
+
+        } else {
+
+            return _sendMail({
+                from: adapterOptions.defaultFrom,
+                subject: adapterOptions.templatesOptions.templates.verifyEmail?.subject || 'Verify Email',
+                to: data.user.get('email') || data.user.get('username'),
+                text: data.link,
+            }, transporter);
+        }
+
+    }
+
+
+    // --- Return Adapter ---
     return Object.freeze({
         sendMail: sendEmail,
         sendPasswordResetEmail: sendPasswordResetEmail,
@@ -15,4 +126,5 @@ let MailAdapter = (adapterOptions: AdapterOptionsInterface) => {
     })
 }
 
+// --- Export Adapter ---
 export default MailAdapter;
